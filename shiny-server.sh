d6d1a6fc1e1a1540c0a6ca119d111d43ab4143c7
nate <n8@adullmoment.com>
2017-07-26 16:12:59 -0400

    skip the activation for now
    need to fix this

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/dumb-init /bin/bash
 if [[ -z $LICENSE ]]; then
   exec /usr/bin/shiny-server 2>&1 &
-  exec /opt/shiny-server/bin/license-manager activate $LICENSE 2>&1
+  #exec /opt/shiny-server/bin/license-manager activate $LICENSE 2>&1
 else
   exec /usr/bin/shiny-server 2>&1
 fi
