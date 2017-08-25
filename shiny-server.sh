2d5592216ac2615f7a2e5ddd4165466eb183f92a
Francesco Pontiggia <pontiggia@g.harvard.edu>
2017-08-25 11:15:53 -0400

    Added rstudio server to the same container and supervisord

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,8 +1,8 @@
-#!/usr/local/bin/dumb-init /bin/bash
+#!/bin/bash
 
 if [[ ! -z $LICENSE ]]; then
   /opt/shiny-server/bin/license-manager activate $LICENSE 2>&1
-  exec /usr/bin/shiny-server 2>&1
+  /usr/bin/shiny-server --server-daemonize 0 2>&1
 else
-  exec /usr/bin/shiny-server 2>&1
+  /usr/bin/shiny-server --server-daemonize 0 2>&1
 fi
